4226bd15cb9e5413afa0eb150311b81c07445a95
nivancheva <nedyalka.tsintsarova@gmail.com>
2023-10-05 11:10:56 +0300

    fix initial advice load

--- src/App.jsx
+++ src/App.jsx
@@ -3,23 +3,30 @@ import './App.css'
 import iconDice from './images/icon-dice.svg';
 import patternDesktop from './images/pattern-divider-desktop.svg';
 import patternMobile from './images/pattern-divider-mobile.svg';
+import { useEffect } from 'react';
 
 function App() {
-  const [showAdvice, setShowAdvice] = useState("");
+  const [showAdvice, setShowAdvice] = useState(null);
 
   async function generateAdvice() {
     const response = await fetch("https://api.adviceslip.com/advice");
     if (response.ok) {
       const advice = await response.json();
-      setShowAdvice(advice.slip.advice);
+      setShowAdvice(advice.slip);
     }  
   }
+
+  useEffect(() => {
+    if (!showAdvice) {
+      generateAdvice();
+    }
+  }, []);
 
   return (
     <div className='wrapper'>
       <div className='card'>
-        <p className='card_advice'>Advice #</p>
-        <p className='card_quote'>{showAdvice ? `"${showAdvice}"` : "" }</p>
+        <p className='card_advice'>Advice #{showAdvice ? showAdvice.id : ""}</p>
+        <p className='card_quote'>{showAdvice ? `"${showAdvice.advice}"` : "" }</p>
         <picture className='pattern_img'>
               <source srcSet={patternDesktop} media="(min-width:800px)"/>
               <img src={patternMobile}/>
