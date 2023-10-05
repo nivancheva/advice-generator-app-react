da170543038e3daf9de1ca820b6b68dd0a4fba57
nivancheva <nedyalka.tsintsarova@gmail.com>
2023-10-05 10:01:20 +0300

    add basic html

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,22 @@
-
 import './App.css'
+import iconDice from './images/icon-dice.svg';
+import patternDesktop from './images/pattern-divider-desktop.svg';
+import patternMobile from './images/pattern-divider-mobile.svg';
 
 function App() {
 
   return (
-    <>
-      Hello
-    </>
+    <div className='wrapper'>
+      <div className='card'>
+        <p>Advice #</p>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Non fuga ullam eveniet illo asperiores, distinctio velit vitae!</p>
+        <picture className='pattern_img'>
+              <source srcSet={patternDesktop} media="(min-width:800px)"/>
+              <img src={patternMobile}/>
+        </picture>
+        <button><img src={iconDice}/></button>
+      </div>
+    </div>
   )
 }
 
